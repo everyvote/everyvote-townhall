3bef6de1655ce40aec2b8a12956ba4ef70801179
Vincent Schramer <vinciple@gmail.com>
2014-11-02 11:52:49 -0600

    cd take 2

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-if test -z "${DEPLOY_BLOB}"; then
+if test -z "${DEPLOY_BLOB_KEY}" -o -z "${DEPLOY_BLOB_URI}"; then
   echo "No deployment configured."
   exit 0;
 fi
 
-echo "${DEPLOY_BLOB}" | openssl enc -d -base64 | tar -xj
+curl -silent "${DEPLOY_BLOB_URI}" | \
+openssl enc -d \
+  -aes-256-cbc -salt \
+  -in "DEPLOY_BLOB" \
+  -k ${DEPLOY_BLOB_KEY} | \
+tar -xj
+  
 bash deploy/start.sh
 
-
+rm -rf deploy
   
