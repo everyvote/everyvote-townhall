773ecc493b2800b09558780462c795fa91d625fe
Vincent Schramer <vinciple@gmail.com>
2014-11-02 12:30:04 -0600

    cd take 3

--- .travis.deploy.sh
+++ .travis.deploy.sh
@@ -5,13 +5,13 @@ if test -z "${DEPLOY_BLOB_KEY}" -o -z "${DEPLOY_BLOB_URI}"; then
   exit 0;
 fi
 
-curl -silent "${DEPLOY_BLOB_URI}" | \
+# Download / decrypt / extract
+curl "${DEPLOY_BLOB_URI}" |
 openssl enc -d \
   -aes-256-cbc -salt \
-  -in "DEPLOY_BLOB" \
-  -k ${DEPLOY_BLOB_KEY} | \
+  -k "${DEPLOY_BLOB_KEY}" | \
 tar -xj
-  
+
 bash deploy/start.sh
 
 rm -rf deploy
