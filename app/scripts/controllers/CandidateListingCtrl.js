5a57211ec1fbdb4d01b586be21678886558e9fdd
Mitch Downey <mdowney@mdowney-macbookpro-2.local>
2014-12-01 23:56:06 -0600

    Begin to mix new HTML with Angular

--- app/scripts/controllers/CandidateListingCtrl.js
+++ app/scripts/controllers/CandidateListingCtrl.js
@@ -9,24 +9,91 @@
  */
 angular.module('everyvoteTuresoApp')
   .controller('CandidateListingCtrl', function ($scope) {
-    
+
     $scope.posts = [
       {
         name: 'President',
-        candidates: ['Sojourner Truth', 'Frederick Douglass', 'Jane Addams', 'Thomas Paine'], 
+        candidates: [
+          {
+            name: 'Sojourner Truth',
+            profile_picture: 'images/ppl/sojournertruth.jpg'
+          },
+          {
+            name: 'Frederick Douglass',
+            profile_picture: 'images/ppl/frederickdouglass.jpg'
+          },
+          {
+            name: 'Jane Addams',
+            profile_picture: 'images/ppl/janeaddams.jpg'
+          },
+          {
+            name: 'Thomas Paine',
+            profile_picture: 'images/ppl/thomaspaine.jpg'
+          }
+        ],
       },
       {
         name: 'Vice-President',
-        candidates: ['Abraham Lincoln', 'Susan B. Anthony', 'Denis Diderot', 'Booker T. Washington'],
+        candidates: [
+          {
+            name: 'Abraham Lincoln',
+            profile_picture: 'images/ppl/abrahamlincoln.jpg'
+          },
+          {
+            name: 'Susan B. Anthony',
+            profile_picture: 'images/ppl/susanbanthony.jpg'
+          },
+          {
+            name: 'Denis Diderot',
+            profile_picture: 'images/ppl/denisdiderot.jpg'
+          },
+          {
+            name: 'Booker T. Washington',
+            profile_picture: 'images/ppl/bookertwashington.jpg'
+          }
+        ],
       },
       {
         name: 'Treasurer',
-        candidates: ['Buckminster Fuller', 'Harriet Tubman', 'James Baldwin', 'Albert Einstein'],
+        candidates: [
+          {
+            name: 'Buckminster Fuller',
+            profile_picture: 'images/ppl/buckminsterfuller.jpg'
+          },
+          {
+            name: 'Harriet Tubman',
+            profile_picture: 'images/ppl/harriettubman.jpg'
+          },
+          {
+            name: 'James Baldwin',
+            profile_picture: 'images/ppl/jamesbaldwin.jpg'
+          },
+          {
+            name: 'Albert Einstein',
+            profile_picture: 'images/ppl/alberteinstein.jpg'
+          }
+        ],
       },
       {
         name: 'Secretary',
-        candidates: ['Marshall McLuhan', 'Mark Twain', 'Martin Luther King Jr.', 'Harriet Beecher Stowe'],
-      }
+        candidates: [
+          {
+            name: 'Marshall McLuhan',
+            profile_picture: 'images/ppl/marshallmcluhan.jpg'
+          },
+          {
+            name: 'Mark Twain',
+            profile_picture: 'images/ppl/marktwain.jpg'
+          },
+          {
+            name: 'Martin Luther King Jr.',
+            profile_picture: 'images/ppl/martinlutherkingjr.jpg'
+          },
+          {
+            name: 'Harriet Beecher Stowe',
+            profile_picture: 'images/ppl/harrietbeecherstowe.jpg'
+          }
+        ],
+      },
     ];
-    
   });
