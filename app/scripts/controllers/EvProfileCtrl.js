3dc3b077bfee069a01e5daf1a478eb2e387f3223
Mitch Downey <mdowney@mdowney-macbookpro-2.local>
2014-12-29 14:16:08 -0600

    Start Social Icon feature

--- app/scripts/controllers/EvProfileCtrl.js
+++ app/scripts/controllers/EvProfileCtrl.js
@@ -20,6 +20,37 @@ angular.module('everyvoteTuresoApp')
         }
         $scope.fullSummary = false;
 
+        // make social link names lowercase to work with Font Awesome icons
+        $scope.socialLinks = candidate.links;
+        var socialLinks = candidate.links,
+            i,
+            isSupported = ["facebook", "twitter", "linkedin", "homepage"];
+
+        for (i = 0; i < socialLinks.length; i++) {
+            $scope.socialLinks[i].note = socialLinks[i].note.toLowerCase();
+            // check if social link icon is supported by Font Awesome
+            console.log(i);
+
+            if (isSupported.indexOf($scope.socialLinks[i].note) > -1) {
+              console.log($scope.socialLinks[i].note);
+              console.log(i);
+              $scope.socialLinks[i].isSupported = true;
+            }
+        }
+
+        // check if "homepage" is a link, then move to front of socialLinks array,
+        // also convert socialLink.note to "home" for Font Awesome compatibility
+        var homePos = $scope.socialLinks.map(function(obj) {
+          return obj.note;
+        }).indexOf("homepage");
+
+
+        if (homePos > -1) {
+          var homeObj = $scope.socialLinks.splice(homePos, 1);
+          homeObj[0].note = "home";
+          $scope.socialLinks.splice(0, 0, homeObj[0]);
+        }
+
         $scope.toggleSummary = function() {
           $scope.fullSummary = (! $scope.fullSummary);
         }
