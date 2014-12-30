4fa838d70922bd5cd0e093429867e9c0393fafc2
Mitch Downey <mdowney@mdowney-macbookpro-2.local>
2014-12-30 16:41:27 -0600

    Develop social icon feature

--- app/scripts/controllers/EvProfileCtrl.js
+++ app/scripts/controllers/EvProfileCtrl.js
@@ -27,28 +27,50 @@ angular.module('everyvoteTuresoApp')
             isSupported = ["facebook", "twitter", "linkedin", "homepage"];
 
         for (i = 0; i < socialLinks.length; i++) {
-            $scope.socialLinks[i].note = socialLinks[i].note.toLowerCase();
-            // check if social link icon is supported by Font Awesome
-            console.log(i);
+            socialLinks[i].note = socialLinks[i].note.toLowerCase();
 
-            if (isSupported.indexOf($scope.socialLinks[i].note) > -1) {
-              console.log($scope.socialLinks[i].note);
-              console.log(i);
-              $scope.socialLinks[i].isSupported = true;
+            // check if social link icon is supported by Font Awesome
+            if (isSupported.indexOf(socialLinks[i].note) > -1) {
+              socialLinks[i].isSupported = true;
             }
         }
 
         // check if "homepage" is a link, then move to front of socialLinks array,
         // also convert socialLink.note to "home" for Font Awesome compatibility
-        var homePos = $scope.socialLinks.map(function(obj) {
+        var homePos = socialLinks.map(function(obj) {
           return obj.note;
         }).indexOf("homepage");
 
 
         if (homePos > -1) {
-          var homeObj = $scope.socialLinks.splice(homePos, 1);
+          var homeObj = socialLinks.splice(homePos, 1);
           homeObj[0].note = "home";
-          $scope.socialLinks.splice(0, 0, homeObj[0]);
+          socialLinks.splice(0, 0, homeObj[0]);
+        }
+
+        // remove socialLinks that are duplicate social media websites/methods
+        // using the ___ shared by ___ on Stack Overflow - ___
+        console.log($scope.socialLinks);
+        $scope.socialLinks = socialLinks.reduce(function(a,b){if(a.indexOf(b)<0)a.push(b);return a;},[]);
+        console.log($scope.socialLinks);
+        $scope.displayedSocialLinks = socialLinks.slice(0,3);
+
+        if ($scope.socialLinks.length > 3) {
+          $scope.showMoreSocialIcon = true;
+        } else {
+          $scope.showMoreSocialIcon = false;
+        }
+
+        $scope.socialIconsAreDisplayed = false;
+
+        $scope.toggleSocialIcons = function() {
+          if ($scope.displayedSocialLinks.length <= 3) {
+            $scope.displayedSocialLinks = $scope.socialLinks;
+            $scope.socialIconsAreDisplayed = true;
+          } else {
+            $scope.displayedSocialLinks = $scope.socialLinks.slice(0,3);
+            $scope.socialIconsAreDisplayed = false;
+          }
         }
 
         $scope.toggleSummary = function() {
