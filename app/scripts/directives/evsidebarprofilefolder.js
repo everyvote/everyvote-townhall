71547f248f1a84bd3ac625cac59d58b23e304c02
Mitch Downey <mdowney@mdowney-macbookpro-2.local>
2015-01-03 12:31:26 -0600

    Add sidebar candidate total

--- app/scripts/directives/evsidebarprofilefolder.js
+++ app/scripts/directives/evsidebarprofilefolder.js
@@ -21,6 +21,8 @@ angular.module('everyvoteTuresoApp')
           $scope.sidebarFolderExpanded = (! $scope.sidebarFolderExpanded);
 
         }
+
+        $scope.post.totalCandidates = $scope.post.candidateIds.length;
       }
     }
   });
