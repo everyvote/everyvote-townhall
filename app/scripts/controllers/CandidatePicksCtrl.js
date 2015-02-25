953abd5f962bce1cabc7ded23d3b8ce4548aa856
Mitch Downey <mitchldowney@gmail.com>
2015-02-24 22:51:04 -0600

    Add non-functioning My Candidates page

--- app/scripts/controllers/CandidatePicksCtrl.js
+++ app/scripts/controllers/CandidatePicksCtrl.js
@@ -8,11 +8,16 @@
  * Controller of the everyvoteTuresoApp
  */
 angular.module('everyvoteTuresoApp')
-  .controller('CandidatePicksCtrl', function ($scope, CandidatePicksService) {
+  .controller('CandidatePicksCtrl', function ($scope, ElectionService) {
 
-    CandidatePicksService.getPicks() /* use something to retrieve person's picks list */
-      .then(function (picks) {
-        $scope.posts = picks.posts;
+    ElectionService.getElection()
+      .then(function (election) {
+        $scope.posts = election.posts;
       });
+
+    // CandidatePicksService.getPicks()
+    //   .then(function (picks) {
+    //     $scope.posts = picks.posts;
+    //   });
 
   });
